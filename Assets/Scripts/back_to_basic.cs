e68512f0ac62f723c15308412f402efe285813e0
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2021-12-02 23:42:12 +0900

    first commit

---
+++ Assets/Scripts/back_to_basic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class back_to_basic : MonoBehaviour
+{
+    public string sceneName1 = "Library";
+    //마우스 터치시
+    void OnMouseDown()
+    {
+        //씬 전환
+        SceneManager.LoadScene(sceneName1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
