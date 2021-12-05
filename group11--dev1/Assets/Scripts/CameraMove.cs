59ebb5da1f9c8b2475225d89f29ba53f317ce4a4
Youngsuh <yschin0508@gmail.com>
2021-12-05 16:53:19 +0900

    download pdf 1page

---
+++ group11--dev1/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Transform playTransform;
+    Vector3 Offset;
+    void Awake()
+    {
+        playTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Offset = transform.position - playTransform.position;
+    }
+    void LateUpdate()
+    { 
+        transform.position = playTransform.position + Offset;
+    }
+}
