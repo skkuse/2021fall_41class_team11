f7a8272fe533f54bcff14a5665c861c507e71e25
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2021-12-12 04:39:02 +0900

    search book

--- Assets/Scripts/MyBall.cs
+++ Assets/Scripts/MyBall.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class MyBall : MonoBehaviour
 {
     Rigidbody rb;
-    float speed = 10f;
+    float speed = 5;
+    float lookSensitivity = 2;
+    float cameraRotationLimit = 45;
+    float currentCameraRotationX;
+    private Camera cam;
  
     void Start()
     {
@@ -14,10 +18,45 @@ public class MyBall : MonoBehaviour
  
     void Update()
     {
+        /*
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
  
         Vector3 getVel = new Vector3(xMove, 0, zMove) * speed;
         rb.velocity = getVel;
+        */
+        Move();
+        //CameraRotate();
+        CharacterRotate();
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        rb.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    void CameraRotate()
+    {
+        float xRotate = Input.GetAxisRaw("Mouse Y");
+        float camaraRotateX = xRotate * lookSensitivity;
+
+        currentCameraRotationX = camaraRotateX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        //cam.transform.localEulterAngles = new Vector3(currentCameraRotationX, 0, 0);
+    }
+
+    void CharacterRotate()
+    {
+        float yRotate = Input.GetAxisRaw("Mouse X");
+        Vector3 rotateY = new Vector3(0, yRotate, 0) * lookSensitivity;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
     }
 }
--- Assets/Scripts/SearchBook.cs
+++ Assets/Scripts/SearchBook.cs
@@ -2,18 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+
+using LitJson;
 
 public class SearchBook : MonoBehaviour
 {
     public GameObject SearchCube;
     public InputField input;
     public Button Button_search;
+    public GameObject SearchResults;
+    public Transform res;
 
+    string jsonResult;
+    bool isOnLoading = true;
+    string Title;
+    bool PdfAvailable = false;
+    string pdfURL;
+    string previewURL;
+
+    IEnumerator LoadData()
+    {
+        string GetDataUrl = "https://www.googleapis.com/books/v1/volumes?q=" + input.text.ToString() + "&orderBy=relevance";// + "&maxResults=10";
+        //Application.OpenURL(GetDataUrl);
+        using (UnityWebRequest www = UnityWebRequest.Get(GetDataUrl))
+        {
+            //www.chunkedTransfer = false;
+            yield return www.Send();
+            if (www.isNetworkError || www.isHttpError) //불러오기 실패 시
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    isOnLoading = false;
+                    jsonResult =
+                        System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    //Debug.Log(jsonResult);
+                    JsonData ItemData = JsonMapper.ToObject(jsonResult);
+                    Title = ItemData["items"][0]["volumeInfo"]["title"].ToString();
+                    Debug.Log(Title);
+                    previewURL = ItemData["items"][0]["volumeInfo"]["previewLink"].ToString();
+                    Application.OpenURL(previewURL);
+                }
+            }
+        }
+    }
     public void SearchButtonClick()
     {
-        if (input)
+        if (input.text.Length > 0)
         {
-            Debug.Log("Search");
+            Debug.Log(input.text);
+            StartCoroutine(LoadData());
+            Debug.Log(pdfURL);
+
+            input.text = "";
         }
     }
 
