bed112d2d9f6a26eda324cf16dc6cb24073de447
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2021-12-12 21:46:44 +0900

    jpg 추가

--- team11/Assets/Scripts/FileLoader.cs
+++ team11/Assets/Scripts/FileLoader.cs
@@ -9,38 +9,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 //using pdf2jpg;
 
-public class FileLoader : MonoBehaviour {
+using System.Drawing;
+// using PQScan.PDFToImage;
+using System.Drawing.Imaging;
+
+public class FileLoader : MonoBehaviour
+{
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         String sourceFilePath = "./Assets/pdfBooks/harrypotter.pdf";
         String outputFolderPath = "./Assets/editPDF";
         PDDocument doc = PDDocument.load(sourceFilePath);
         DirectoryInfo di = new DirectoryInfo(outputFolderPath);
-        if(di.Exists ==false) di.Create();
+        if (di.Exists == false) di.Create();
         Splitter splitter = new Splitter();
         java.util.List pages = splitter.split(doc);
         PDDocument docu1 = (PDDocument)pages.get(1);
-        PDDocument docu2 = (PDDocument)pages.get(1);
-        docu1.save(outputFolderPath+"/"+"test1.pdf");
-        docu2.save(outputFolderPath+"/"+"test2.pdf");
+        PDDocument docu2 = (PDDocument)pages.get(2);
+        docu1.save(outputFolderPath + "/" + "test1.pdf");
+        docu2.save(outputFolderPath + "/" + "test2.pdf");
+
+        
         //pdf2jpg1 pdf2j = new pdf2jpg1();
         //pdf2j.change(outputFolderPath+"/"+"test1.pdf",outputFolderPath);
         docu1.close();
         docu2.close();
 
-        
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
 
 }
-
